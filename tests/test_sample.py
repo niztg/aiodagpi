3e6153088abf7497807b075492816d447cbb5596
Raj Sharma <yrsharma@icloud.com>
2020-09-30 22:25:04 +0100

    Final Travis

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,6 @@
 import aiodagpi
 
-e = ''
-
 # Function travis will check
 # This will be changed when I actually have functionality
 def test():
-    assert e == 'False', 'this should return false, i am testing'
+    assert True, 'True != True'
